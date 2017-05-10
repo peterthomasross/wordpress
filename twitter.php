cd1dafc3b80a81a31b95af071d1202c814bcf7b6
peterthomasross <peter.ross@live.com>
2017-05-10 15:02:33 +0100

    Update twitter.php

--- twitter.php
+++ twitter.php
@@ -28,6 +28,7 @@ THE SOFTWARE.
  */
 /*
 Plugin Name: Twitter
+GitHub Plugin URI: https://github.com/peterthomasross/wordpress
 Plugin URI:  http://wordpress.org/plugins/twitter/
 Description: Official Twitter plugin for WordPress. Embed Twitter content and grow your audience on Twitter. Requires PHP 5.4 or greater.
 Version:     2.0.2
